3b83304704f283af07fefcdbba9e79fd1f3ad36a
Nickr <romero.nicholejohn@gmail.com>
2022-05-16 07:46:27 +0800

    add packages and build for android

--- frontend/ionic-okta/config/stg/environment.ts
+++ frontend/ionic-okta/config/stg/environment.ts
@@ -2,6 +2,7 @@ export const environment = {
     production: false,
     
     okta_client_id: '0oa4lp2wypUZvhu9v5d7',
+    okta_mobile_client_id: '0oa50l29hbN8BfsNY5d7',
     okta_issuer: 'https://dev-88557974.okta.com/oauth2/default',
     okta_pkce: true,
     okta_host: 'dev-88557974.okta.com',
--- frontend/ionic-okta/src/environments/environment.ts
+++ frontend/ionic-okta/src/environments/environment.ts
@@ -6,5 +6,13 @@ export const environment = {
     okta_pkce: true,
     okta_host: 'dev-88557974.okta.com',
     okta_redirect_uri: 'http://localhost:8100',
-    okta_logout_uri: 'http://localhost:8100'
+    okta_logout_uri: 'http://localhost:8100',
+    okta_scopes: ['openid', 'profile', 'email'],
+    okta_response_mode: 'query',
+
+    okta_mobile_client_id: '0oa50l29hbN8BfsNY5d7',
+    okta_mobile_redirect_uri: 'http://localhost:8100/signin',
+    okta_mobile_logout_uri: 'http://localhost:8100/signout',
+    okta_mobile_response_type: 'code',
+    okta_mobile_scopes: ['openid', 'profile', 'email', 'offline_access', 'device_sso']
 }
