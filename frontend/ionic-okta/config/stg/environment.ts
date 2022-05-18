fb5b65a7cebd874c5ac0d8f7325b3fe815778598
Nichole John Romero <nickr@Nicholes-MacBook-Air.local>
2022-05-19 04:43:51 +0800

    clear config

--- frontend/ionic-okta/config/stg/environment.ts
+++ frontend/ionic-okta/config/stg/environment.ts
@@ -1,11 +1,18 @@
 export const environment = {
     production: false,
     
-    okta_client_id: '0oa4lp2wypUZvhu9v5d7',
-    okta_mobile_client_id: '0oa50l29hbN8BfsNY5d7',
-    okta_issuer: 'https://dev-88557974.okta.com/oauth2/default',
+    okta_client_id: '',
+    okta_issuer: '',
     okta_pkce: true,
-    okta_host: 'dev-88557974.okta.com',
+    okta_host: '',
     okta_redirect_uri: 'http://localhost:8100',
-    okta_logout_uri: 'http://localhost:8100'
+    okta_logout_uri: 'http://localhost:8100',
+    okta_scopes: ['openid', 'profile', 'email'],
+    okta_response_mode: 'query',
+
+    okta_mobile_client_id: '',
+    okta_mobile_redirect_uri: 'http://localhost:8100/signin',
+    okta_mobile_logout_uri: 'http://localhost:8100/signout',
+    okta_mobile_response_type: 'code',
+    okta_mobile_scopes: ['openid', 'profile', 'email', 'offline_access', 'device_sso']
 }
